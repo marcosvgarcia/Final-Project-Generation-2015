b9ad575a439c205b17dffa8e445fec8269f5c275
marcosvgarcia <marcosvazquezgarcia@outlook.es>
2015-06-10 15:14:06 +0200

    SEARCH BOX
    whit this new implementation I can do my weather search with a double
    click on the map and with an imput on the search box from the map header.

--- app/assets/javascripts/weather.js
+++ app/assets/javascripts/weather.js
@@ -3,7 +3,10 @@
 $(document).ready(function() {
 
 	initialize();
+
 	var map;
+	var input;
+	var searchBox;
 	var lat;
 	var lng;
 
@@ -14,7 +17,13 @@ $(document).ready(function() {
 	  		disableDoubleClickZoom: true
 		};
 		
-		map = new google.maps.Map(document.getElementById("location"), mapOptions);
+		map = new google.maps.Map(document.getElementById("location"), mapOptions, {mapTypeId: google.maps.MapTypeId.ROADMAP});
+
+		input = document.getElementById("pac-input");
+  
+  		map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+  		searchBox = new google.maps.places.SearchBox(input);
 	}
 
 	google.maps.event.addListener(map, "dblclick", function(event) {
@@ -45,7 +54,7 @@ $(document).ready(function() {
 						   "</li><li>HUMIDITY: " + current.humidity + 
 						   "</li><li>WIND: " + current.wind + 
 						   "</li><li>RADIATION: " + current.sun + "</li>" +
-						   "</li><li>WATER PRODUCTION: " + water_production(current) + "</li>");
+						   "</li><li>WATER PRODUCTION: " + water_production(current) + " L</li>");
 		}
 
 		function water_production(current) {
@@ -60,7 +69,69 @@ $(document).ready(function() {
 
 			return water_litres;
 		}
-
 	});
+
+	google.maps.event.addListener(searchBox, 'places_changed', function() {
+    	
+    	var places = searchBox.getPlaces();
+
+    	if (places.length == 0) {
+      		return;
+    	}
+
+    	var bounds = new google.maps.LatLngBounds();
+
+    	for (var i = 0, place; place = places[i]; i++) {
+
+      		bounds.extend(place.geometry.location);
+    	}
+
+    	map.fitBounds(bounds);
+
+    	latObj = bounds["za"];
+    	lat = latObj["A"];
+
+    	lngObj = bounds["qa"];
+    	lng = lngObj["A"];
+
+    	coordinates = {latitude: lat,
+    				   longitude: lng };
+
+    	$.ajax({
+			url: "http://localhost:3000/weather",
+			data: coordinates,
+			success: function(response){console.log("OK"); conditions_ul(response) },
+			error: function(){console.log("WRONG")},
+			dataType: "json"
+
+		});
+
+
+		function conditions_ul(current) {
+
+			$("ul").empty();
+			
+			$("ul").append("<li>LOCATION: " + current.country + ", " + current.city + 
+						   "</li><li>TEMPERATURE (ºF): " + current.fahrenheit + 
+						   "</li><li>TEMPERATURE (ºC): " + current.celsius + 
+						   "</li><li>HUMIDITY: " + current.humidity + 
+						   "</li><li>WIND: " + current.wind + 
+						   "</li><li>RADIATION: " + current.sun + "</li>" +
+						   "</li><li>WATER PRODUCTION: " + water_production(current) + " L</li>");
+		}
+
+		function water_production(current) {
+
+			var h = parseFloat(current.humidity);
+
+			var k_humidity = (5 * Math.pow(10, -5) * Math.pow(h, 2)) + (0.013 * h) + 0.0277;
+
+			var k_temperature = (-0.0002 * Math.pow(current.celsius, 2)) + (0.0458 * current.celsius) - 0.1029;
+
+			var water_litres = k_humidity * k_temperature * 100;
+
+			return water_litres;
+		}
+    });
 });
 
